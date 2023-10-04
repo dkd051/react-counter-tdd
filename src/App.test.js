01ec133f47b2247821a77877009d3ec9680fd4cf
Koo <gokinyu4@gmail.com>
2023-10-04 13:08:13 +0900

    카운터의 더하기 버튼 클릭시 값이 0에서 1로 변경되는지 확인 카운터의 더하기 버튼 클릭시 값이 0에서 -1로 변경되는지 확인

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('the counter starts at 0', () => {
@@ -6,7 +6,7 @@ test('the counter starts at 0', () => {
   // screen object를 이용해서 원하는 엘리먼트에 접근(접근할 때 ID로)
   const counterElement = screen.getByTestId('counter');
   // id가 counter인 엘리먼트의 텍스트가 0인지 테스트
-  expect(counterElement).toHaveTextContent(0);
+  expect(counterElement).toHaveTextContent('0');
 });
 
 test('minus button has correct text', () => {
@@ -22,3 +22,19 @@ test('plus button has correct text', () => {
   const plusButtonElement = screen.getByTestId('plus-button');
   expect(plusButtonElement).toHaveTextContent('+');
 });
+
+test('when the + button is pressed, the counter changes to 1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('plus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent('1');
+})
+
+test('when the - button is pressed, the counter changes to -1', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('minus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent('-1');
+})
